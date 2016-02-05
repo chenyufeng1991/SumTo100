faf69918926ff87d27f7e98ae656fdd0e4ec6232
chenyufeng <503434355@qq.com>
2016-02-05 16:23:21 +0800

    complete

--- SumTo100/main.c
+++ SumTo100/main.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>
 
+/**
+ *  要求找出数组中两个数相加为100的所有组合；
+ */
+
+void findSumTo100(int *a,int n);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+
+    int a[] = {55,50,99,80,1,30,70};
+    //排序后的数组，
+    int a_sort[] = {1,30,50,55,70,80,99};
+    findSumTo100(a_sort,7);
+
     return 0;
+}
+
+void findSumTo100(int *a,int n){
+
+
+    for (int i = 0,j = n - 1; i != j; ) {
+
+        int sum = a[i] + a[j];
+        if (sum == 100) {
+            printf("%d %d\n",a[i],a[j]);
+            i++;
+            j--;
+        }
+        else if (sum < 100){
+            i++;
+        }
+        else if (sum > 100){
+            j--;
+        }
+    }
 }
